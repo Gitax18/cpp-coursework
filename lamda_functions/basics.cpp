436a6ffc4ea94f74aa3866fa38cd8f47cbaa2447
Gitax18 <lusifer0418@gmail.com>
2024-04-07 21:13:36 +0530

    no comments needed

--- lamda_functions/basics.cpp
+++ lamda_functions/basics.cpp
@@ -1,23 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
+  
 
 int main(){
   auto squareAr = [](int a){return a*a;};
   cout << squareAr(5) << endl;
 
   int x = 5;
-  /*
-  auto add = [](int a){
-    return x+a;   // here x is not accessible.
-  };
-  return 0;
-  */
+  
+  // auto add = [](int a){
+  //   return x+a;   // here x is not accessible.
+  // };
+   
 
-  auto add = [x](int a){  // to access global/static variable, we've to pass them in capture list.
+  auto add = [x](int a){  // to access outer variable, we've to pass them in capture list.
    return x+a;
   };
-  cout << add(10) << endl;
 
+  cout << add(10) << endl;
 
   return 0;
 }
