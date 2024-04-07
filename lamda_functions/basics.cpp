ee02be676444b0be43f5ec9017331fba805ff03b
Gitax18 <lusifer0418@gmail.com>
2024-04-07 20:59:35 +0530

    lambda functions

---
+++ lamda_functions/basics.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  auto squareAr = [](int a){return a*a;};
+  cout << squareAr(5) << endl;
+
+  int x = 5;
+  /*
+  auto add = [](int a){
+    return x+a;   // here x is not accessible.
+  };
+  return 0;
+  */
+
+  auto add = [x](int a){  // to access global/static variable, we've to pass them in capture list.
+   return x+a;
+  };
+  cout << add(10) << endl;
+
+
+  return 0;
+}
